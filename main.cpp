2b845aea9869801ff7bc8473ce5b82178bdd3927
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-05-07 00:33:01 +0900

    ベース

--- main.cpp
+++ main.cpp
@@ -7,25 +7,7 @@ struct Vector3 {
 	float x, y, z;
 };
 
-//加算
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-	Vector3 m3;
-	m3.x = v1.x + v2.x;
-	m3.y = v1.y + v2.y;
-	m3.z = v1.z + v2.z;
-	
-	return m3;
 
-};
-//減算
-Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
-	Vector3 m3;
-	m3.x = v1.x - v2.x;
-	m3.y = v1.y - v2.y;
-	m3.z = v1.z - v2.z;
-	return m3;
-};
-//スカラー倍
 Vector3 Multiply(float scalar, const Vector3& v) {
 	Vector3 m3;
 	m3.x = v.x * scalar;
@@ -34,36 +16,6 @@ Vector3 Multiply(float scalar, const Vector3& v) {
 
 	return m3;
 };
-
-//内績
-float Dot(const Vector3& v1, const Vector3& v2) {
-	float m3;
-	m3 = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-
-
-	return m3;
-};
-
-//長さ（ノルム）
-float Length(const Vector3& v) {
-
-	float m3 =  sqrtf(v.x * v.x)+sqrtf(v.y * v.y)+sqrtf(v.z * v.z);
-	
-
-	return m3;
-};
-
-//正規化
-Vector3 Normalize(const Vector3& v);
-
-static const int kColumnWidth = 60;
-static const int kRowHeight = 20;
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
-	Novice::ScreenPrintf(x,y,"%.02f",vector.x);
-	Novice::ScreenPrintf(x+kColumnWidth,y,"%.02f",vector.y);
-	Novice::ScreenPrintf(x+kColumnWidth*2,y,"%.02f",vector.z);
-	Novice::ScreenPrintf(x+kColumnWidth*3,y,"%s",label);
-}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -75,11 +27,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 	
-	//初期化
-	Vector3 v1{ 1.0f,3.0f,-5.0f };
-	Vector3 v2{ 4.0f,-1.0f,2.0f };
-	float k = { 4.0f };
-
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -93,14 +40,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1,v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Normalize(v2);
-
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -109,12 +48,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, resultAdd, " : Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
-		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, " : Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%.02f  : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%.02f  : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, "  : Normalize");
 
 		///
 		/// ↑描画処理ここまで
