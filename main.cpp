a5247ce1f25f4a2bb57fc8ab681fd69e9b8a69b8
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-05-08 00:52:05 +0900

    完成？

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <Novice.h>
-#include<math.h>
+#include<cmath>
 
 const char kWindowTitle[] = "LE2B_05_オイカワユウマ";
 
 struct Vector3 {
 	float x, y, z;
+};
+struct Matrix4x4 {
+	float m[4][4];
 };
 
 
@@ -17,6 +20,121 @@ Vector3 Multiply(float scalar, const Vector3& v) {
 	return m3;
 };
 
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Matrix4x4& rotate, const Vector3& translate) {
+	Matrix4x4 m4;
+	m4.m[0][0] = rotate.m[0][0] * scale.x;
+	m4.m[0][1] = rotate.m[0][1] * scale.x;
+	m4.m[0][2] = rotate.m[0][2] * scale.x;
+
+	m4.m[1][0] = rotate.m[1][0] * scale.y;
+	m4.m[1][1] = rotate.m[1][1] * scale.y;
+	m4.m[1][2] = rotate.m[1][2] * scale.y;
+
+	m4.m[2][0] = rotate.m[2][0] * scale.z;
+	m4.m[2][1] = rotate.m[2][1] * scale.z;
+	m4.m[2][2] = rotate.m[2][2] * scale.z;
+
+
+	m4.m[3][0] = translate.x;
+	m4.m[3][1] = translate.y;
+	m4.m[3][2] = translate.z;
+	m4.m[0][3] = 0;
+	m4.m[1][3] = 0;
+	m4.m[2][3] = 0;
+	m4.m[3][3] = 1.0f;
+
+	return m4;
+};
+
+Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
+	Matrix4x4 m4;
+	m4.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
+	m4.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
+	m4.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
+	m4.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3];
+
+	m4.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0];
+	m4.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1];
+	m4.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2];
+	m4.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3];
+
+	m4.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0];
+	m4.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1];
+	m4.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2];
+	m4.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
+
+	m4.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] + m1.m[3][3] * m2.m[3][0];
+	m4.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1];
+	m4.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2];
+	m4.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
+	return m4;
+};
+
+// 1. X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 m4;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m4.m[i][j] = 0;
+		}
+	}
+	m4.m[0][0] = 1;
+	m4.m[1][1] = std::cos(radian);
+	m4.m[1][2] = std::sin(radian);
+	m4.m[2][1] = -std::sin(radian);
+	m4.m[2][2] = std::cos(radian);
+	m4.m[3][3] = 1;
+	return m4;
+};
+
+// 2. Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 m4;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m4.m[i][j] = 0;
+		}
+	}
+	m4.m[0][0] = std::cos(radian);
+	m4.m[1][1] = 1;
+	m4.m[2][2] = std::cos(radian);
+	m4.m[0][2] = -std::sin(radian);
+	m4.m[2][0] = std::sin(radian);
+	m4.m[3][3] = 1;
+	return m4;
+};
+
+// 3. Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 m4;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m4.m[i][j] = 0;
+		}
+	}
+	m4.m[0][0] = std::cos(radian);
+	m4.m[2][2] = 1;
+	m4.m[1][1] = std::cos(radian);
+	m4.m[1][0] = -std::sin(radian);
+	m4.m[0][1] = std::sin(radian);
+	m4.m[3][3] = 1;
+	return m4;
+};;
+
+
+static const int kColumnWidth = 60;
+static const int kRowHeight = 20;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight + 20, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -26,6 +144,21 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	//初期化
+	
+	Vector3 scale{ 1.2f,0.79f,-2.1f };
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Vector3 translate{ 2.7f,-4.15f,1.57f };
+
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotateXYZMatrix, translate);
+
+
 	
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -48,7 +181,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-
+		MatrixScreenPrintf(0, 0, worldMatrix, "worldMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
