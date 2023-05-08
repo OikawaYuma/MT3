fbd293eb429055b5f880ca2d7933dff27598500d
OikawaYuma <k022g0016@g.neec.ac.jp>
2023-05-08 10:08:52 +0900

    完成予定

--- main.cpp
+++ main.cpp
@@ -20,33 +20,6 @@ Vector3 Multiply(float scalar, const Vector3& v) {
 	return m3;
 };
 
-// 3次元アフィン変換行列
-Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Matrix4x4& rotate, const Vector3& translate) {
-	Matrix4x4 m4;
-	m4.m[0][0] = rotate.m[0][0] * scale.x;
-	m4.m[0][1] = rotate.m[0][1] * scale.x;
-	m4.m[0][2] = rotate.m[0][2] * scale.x;
-
-	m4.m[1][0] = rotate.m[1][0] * scale.y;
-	m4.m[1][1] = rotate.m[1][1] * scale.y;
-	m4.m[1][2] = rotate.m[1][2] * scale.y;
-
-	m4.m[2][0] = rotate.m[2][0] * scale.z;
-	m4.m[2][1] = rotate.m[2][1] * scale.z;
-	m4.m[2][2] = rotate.m[2][2] * scale.z;
-
-
-	m4.m[3][0] = translate.x;
-	m4.m[3][1] = translate.y;
-	m4.m[3][2] = translate.z;
-	m4.m[0][3] = 0;
-	m4.m[1][3] = 0;
-	m4.m[2][3] = 0;
-	m4.m[3][3] = 1.0f;
-
-	return m4;
-};
-
 Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 	Matrix4x4 m4;
 	m4.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
@@ -70,6 +43,78 @@ Matrix4x4 Multiply(const Matrix4x4 m1, const Matrix4x4 m2) {
 	m4.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
 	return m4;
 };
+
+// 3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate) {
+	Matrix4x4 m4;
+	Matrix4x4 m1;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m1.m[i][j] = 0;
+		}
+	}
+	m1.m[0][0] = 1;
+	m1.m[1][1] = std::cos(rotate.x);
+	m1.m[1][2] = std::sin(rotate.x);
+	m1.m[2][1] = -std::sin(rotate.x);
+	m1.m[2][2] = std::cos(rotate.x);
+	m1.m[3][3] = 1;
+
+	Matrix4x4 m2;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m2.m[i][j] = 0;
+		}
+	}
+	m2.m[0][0] = std::cos(rotate.y);
+	m2.m[1][1] = 1;
+	m2.m[2][2] = std::cos(rotate.y);
+	m2.m[0][2] = -std::sin(rotate.y);
+	m2.m[2][0] = std::sin(rotate.y);
+	m2.m[3][3] = 1;
+
+	Matrix4x4 m3;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m3.m[i][j] = 0;
+		}
+	}
+	m3.m[0][0] = std::cos(rotate.z);
+	m3.m[2][2] = 1;
+	m3.m[1][1] = std::cos(rotate.z);
+	m3.m[1][0] = -std::sin(rotate.z);
+	m3.m[0][1] = std::sin(rotate.z);
+	m3.m[3][3] = 1;
+
+	Matrix4x4 xyz = Multiply(m1, Multiply(m2, m3));
+
+
+
+	m4.m[0][0] = xyz.m[0][0] * scale.x;
+	m4.m[0][1] = xyz.m[0][1] * scale.x;
+	m4.m[0][2] = xyz.m[0][2] * scale.x;
+
+	m4.m[1][0] = xyz.m[1][0] * scale.y;
+	m4.m[1][1] = xyz.m[1][1] * scale.y;
+	m4.m[1][2] = xyz.m[1][2] * scale.y;
+
+	m4.m[2][0] = xyz.m[2][0] * scale.z;
+	m4.m[2][1] = xyz.m[2][1] * scale.z;
+	m4.m[2][2] = xyz.m[2][2] * scale.z;
+
+
+	m4.m[3][0] = translate.x;
+	m4.m[3][1] = translate.y;
+	m4.m[3][2] = translate.z;
+	m4.m[0][3] = 0;
+	m4.m[1][3] = 0;
+	m4.m[2][3] = 0;
+	m4.m[3][3] = 1.0f;
+
+	return m4;
+};
+
+
 
 // 1. X軸回転行列
 Matrix4x4 MakeRotateXMatrix(float radian) {
@@ -156,7 +201,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
 	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
 
-	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotateXYZMatrix, translate);
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 
 	
